acf51bc0bf9c31980ae2145bacb325d609c3591a
tangle <vantangst@gmail.com>
2023-05-13 14:50:09 +0700

    configuration network data

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,39 @@
 package com.udacity.asteroidradar.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Constants
+import com.udacity.asteroidradar.database.getDatabase
+import com.udacity.asteroidradar.repository.AsteroidRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val database = getDatabase(application)
+    private val asteroidRepository = AsteroidRepository(database)
+
+    init {
+        getFeed()
+    }
+
+    private fun getFeed() {
+        viewModelScope.launch(Dispatchers.IO) {
+            asteroidRepository.refreshFeed(getDate(), getDate(Constants.DEFAULT_END_DATE_DAYS), Constants.API_KEY)
+        }
+    }
+
+    private fun getDate(plush: Int? = null): String {
+        val calendar = Calendar.getInstance()
+        plush?.let {
+            calendar.add(Calendar.DAY_OF_YEAR, plush)
+        }
+        val currentTime = calendar.time
+        val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+        return dateFormat.format(currentTime)
+    }
 }
